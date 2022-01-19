222f08d9009e6fbea49b4f6b832f482bea45bd55
Marcus Yoda <marcus@Acelera.cloud>
2022-01-19 18:49:58 -0300

    v1.0.3

---
+++ tests/helper.test.ts
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+
+describe('Colors', function() {
+  it('Must has info color', function() {
+    const result = (5 + 2);
+    expect(result).equal(7);
+  });
+  it('Must has notice color', function() {
+    const result = (5 + 2);
+    expect(result).equal(7);
+  });
+  it('Must has warn color', function() {
+    const result = (5 + 2);
+    expect(result).equal(7);
+  });
+  it('Must has err color', function() {
+    const result = (5 + 2);
+    expect(result).equal(7);
+  });
+  it('Must has fail color', function() {
+    const result = (5 + 2);
+    expect(result).equal(7);
+  });
+});
