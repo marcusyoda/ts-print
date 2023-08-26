af991ddfac6bf324c5015b1554d574ee0f85f99d
Marcus Yoda <marcus@acelera.cloud>
2023-08-25 21:08:18 -0300

    feat(build): updated config and strategy

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+
 export default [
   {
     input: './src/index.ts',
@@ -6,7 +8,7 @@ export default [
       file: './lib/index.esm.js',
       format: 'esm',
     },
-    plugins: [typescript()],
+    plugins: [typescript(), commonjs()],
   },
   {
     input: './src/index.ts',
@@ -14,6 +16,6 @@ export default [
       file: './lib/index.js',
       format: 'cjs',
     },
-    plugins: [typescript()],
+    plugins: [typescript(), commonjs()],
   },
 ]
