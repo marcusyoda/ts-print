e477c2ab53313c790609a38c75c587dd3198ab23
Marcus Yoda <marcus@acelera.cloud>
2023-10-07 15:52:12 -0300

    feat(node-18): update to node 18 and add tests to actions

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,12 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts'],
   collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
